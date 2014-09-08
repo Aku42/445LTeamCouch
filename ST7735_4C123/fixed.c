bb608b2e56096d3f668722686f9a7f0c33dbe50a
Aku42 <eljiminator@gmail.com>
2014-09-08 01:20:38 -0500

    TM4C123 Lab 1 tested
    lab 1 working with all but the binary (/256) functions fully tested
    current test main() function is the test case for the signed functions

--- ST7735_4C123/fixed.c
+++ ST7735_4C123/fixed.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include "ST7735.h"
 #include "fixed.h"
 
 void Fixed_uDecOut2s(uint32_t n,  char *string){
 	int32_t i,j;
-	if(!((j=(int)log10((double)n))<3)){
-		j = 3;
+	if(((j=(int)log10((double)n))<3)){
+		j = 2;
 	}
-	for(i = j; i >= 0; i--){
+	j += 1;
+	for(i = 5; i >= 5-j; i--){
 		if(i == 3){
 			string[i] = '.';
 		}
 		else{
-			string[i] = (n%10) + 48;
+			string[i] = (n%10) + 0x30;
 			n = n/10;
 		}
 	}
@@ -22,8 +25,10 @@ void Fixed_uDecOut2s(uint32_t n,  char *string){
 
 void Fixed_uDecOut2(uint32_t n){
 	uint8_t *str;
+	str = (uint8_t*)malloc(sizeof(uint8_t)*7);
+	str = (uint8_t*)strcpy((char*)str, "      ");
 	if(n > 99999){
-		str = (uint8_t*)"***.**";
+		str = (uint8_t*)strcpy((char*)str, "***.**");
 	}
 	else{
 		Fixed_uDecOut2s(n, (char*)str);
@@ -34,14 +39,17 @@ void Fixed_uDecOut2(uint32_t n){
 void Fixed_uDecOut3(uint32_t n){
 	uint8_t *str;
 	int32_t i,j;
+	str = (uint8_t*)malloc(sizeof(uint8_t)*7);
+	str = (uint8_t*)strcpy((char*)str, "      ");
 	if(n > 99999){
-		str = (uint8_t*)"**.***";
+		str = (uint8_t*)strcpy((char*)str, "**.***");
 	}
 	else{
-		if(!((j=(int)log10((double)n))<4)){
-		  j = 4;
+		if((j=(int)log10((double)n))<4){
+		  j = 3;
 	  }
-		for(i = j; i >= 0; i--){
+		j++;
+		for(i = 5; i >= 5-j; i--){
 			if(i == 2){
 				str[i] = '.';
 			}
@@ -56,30 +64,30 @@ void Fixed_uDecOut3(uint32_t n){
 
 void Fixed_sDecOut3s(int32_t n, char *string){
 	int32_t i, j;
-	if(!((j=(int)log10((double)n))<4)){
-		j=4;
+	if((j=(int)log10((double)n))<3){
+		j=3;
 	}
-	if(n<0){
-		j+=1;
+	j++;
+	if(n < 0){
+		string[0] = '-';
 	}
-	for(i = j; i >= 1; i--){
-		if(i == j-3){
+	for(i = 5; i >= 5-j; i--){
+		if(i == 2){
 			string[i] = '.';
 		}
 		else{
-			string[i] = (n%10) + 48;
+			string[i] = (abs(n)%10) + 48;
 			n = n/10;
 		}
-	}
-	if(n < 0){
-		string[0] = '-';
 	}
 }
 
 void Fixed_sDecOut3(int32_t n){
 	uint8_t *str;
+	str = (uint8_t*)malloc(sizeof(uint8_t)*7);
+	str = (uint8_t*)strcpy((char*)str, "      ");
 	if((n > 9999)||(n < -9999)){
-		str = (uint8_t*)"*.***";
+		str = (uint8_t*)strcpy((char*)str, " *.***");
 	}
 	else{
 		Fixed_sDecOut3s(n, (char*)str);
@@ -90,10 +98,11 @@ void Fixed_sDecOut3(int32_t n){
 void Fixed_uBinOut8s(uint32_t n,  char *string){
 	int32_t i,j;
 	n = ((100*n)+128)/256;
-	if(!((j=(int)log10((double)n))<3)){
+	if((j=(int)log10((double)n))<3){
 		j=3;
 	}
-	for(i = j; i >= 0; i--){
+	j++;
+	for(i = 5; i >= 0; i--){
 		if(i == 3){
 			string[i] = '.';
 		}
@@ -106,8 +115,10 @@ void Fixed_uBinOut8s(uint32_t n,  char *string){
 
 void Fixed_uBinOut8(uint32_t n){
 	uint8_t *str;
+	str = (uint8_t*)malloc(sizeof(uint8_t)*7);
+	str = (uint8_t*)strcpy((char*)str, "      ");
 	if(n >= 256000){
-		str = (uint8_t*)"***.**";
+		str = (uint8_t*)strcpy((char*)str, "***.**");
 	}
 	else{
 		Fixed_uBinOut8s(n, (char*)str);
--- ST7735_4C123/fixed.d
+++ ST7735_4C123/fixed.d
@@ -2,5 +2,7 @@
 .\fixed.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
 .\fixed.o: C:\Keil\ARM\ARMCC\bin\..\include\math.h
 .\fixed.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\fixed.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\fixed.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
 .\fixed.o: ST7735.h
 .\fixed.o: fixed.h
